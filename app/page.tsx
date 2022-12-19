cf0c1d178a5bc7be5dfe4f833bfa5aa54f891d5c
Felix Orinda <forinda82@gmail.com>
2022-12-19 17:14:10 +0300

    Add appDir setup

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from 'next'
+import './../styles/globals.css'
+import React from 'react'
+
+const Home:NextPage = () => {
+  return (
+    <div>Home</div>
+  )
+}
+
+export default Home
